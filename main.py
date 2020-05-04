d697b3e6f929e1a5960dcf47a8389b0c748efeb5
francomanca93 <francomanca93@gmail.com>
2020-05-04 15:00:58 -0300

    Registrando eventos del teclado y cambios menores

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import sys, pygame
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 
 pygame.init()  # Inicializa todos los modulos pygame importados
 
 # ---------- Settings ----------
-size = width, height = 600, 600  # Tamaño de pantalla
+size = width, height = 650, 650  # Tamaño de pantalla
+
+background = 25, 25, 25  # Color de fondo
+color_cell = (128, 128, 128) # Color de la rejilla de las celdas
+width_line_cell = 1  # Ancho de la linea para las celdas
+screen = pygame.display.set_mode(size)  # Creacion de la pantalla
+
+screen.fill(background)  #  Pintamos el fondo con el color elegido
 
 # Definiendo cantidad de celdas que tendra la pantalla
-numberX_cells = 60  # Numero de celdas en x
-numberY_cells = 60  # Numero de celdas en y
+numberX_cells = 50  # Numero de celdas en x
+numberY_cells = 50  # Numero de celdas en y
 
 # Dimension de celdas
 dimensionCell_width = (width - 1) / numberX_cells
 dimensionCell_height = (height- 1) / numberY_cells
 
-background = 25, 25, 25  # Color de fondo
-color_cell = (128, 128, 128) # Color de la rejilla de las celdas
-width_line_cell = 1  # Ancho de la linea para las celdas
-screen = pygame.display.set_mode(size)  # Pantalla
-
-screen.fill(background)  #  
-
+# ------------------------------------------------------------
+# Estado de las celdas. Vivas = 1; Muertas = 0
 # Inicializamos todas las celdas en 0
 gameState = np.zeros((numberX_cells, numberY_cells))
 
@@ -32,56 +33,70 @@ gameState[22, 22] = 1
 gameState[22, 23] = 1
 gameState[21, 23] = 1
 gameState[20, 23] = 1
+# -----------------------------------------------------------
+
+# Control de la ejecución del juego
+pauseExect = False
 
 while True:
-
-    # Para cerrar la ventana desde la x superior
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # This event gets me access to close the window
-            sys.exit()
 
     # Actualizo estado. Nuevo estado sera una copia de gameState
     new_gameState = np.copy(gameState)
 
-    # Actualizo la pantalla. Para ver el movimiento del automata celular
+    # Actualizo la pantalla. La limpio del estado anterior, para que la informacion no se superponga
     screen.fill(background)
+    time.sleep(0.1)
+
+    # Registramos eventos del teclado y raton.
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  # This event gets me access to close the window
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            pauseExect = not pauseExect
 
     # Creando rejilla
     for y in range(0, numberY_cells):
         for x in range(0, numberY_cells):
             
-            # Calculo el numero de vecinos alrededor de la celda dada por el ciclo
-            n_neigh = gameState[(x - 1) % numberX_cells, (y - 1) % numberY_cells] + \
-                      gameState[(x    ) % numberX_cells, (y - 1) % numberY_cells] + \
-                      gameState[(x + 1) % numberX_cells, (y - 1) % numberY_cells] + \
-                      gameState[(x - 1) % numberX_cells, (y    ) % numberY_cells] + \
-                      gameState[(x + 1) % numberX_cells, (y    ) % numberY_cells] + \
-                      gameState[(x - 1) % numberX_cells, (y + 1) % numberY_cells] + \
-                      gameState[(x    ) % numberX_cells, (y + 1) % numberY_cells] + \
-                      gameState[(x + 1) % numberX_cells, (y + 1) % numberY_cells]
-
-            # ----- REGLAS DEL JUEGO DE LA VIDA -----
-
-            # Una célula muerta con exactamente 3 células vecinas vivas "nace".
-            if gameState[x, y] == 0 and n_neigh == 3:
-                new_gameState[x, y] = 1
-            # Una célula viva con 2 o 3 células vecinas vivas sigue viva, en otro caso muere.
-            elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
-                new_gameState[x, y] = 0
-            
+            if not pauseExect:
+                # Calculo el numero de vecinos alrededor de la celda dada por el ciclo
+                # Con la operación modulo me permite simular que si salgo de la pantalla aparezco por la otra parte
+                n_neigh = gameState[(x - 1) % numberX_cells, (y - 1) % numberY_cells] + \
+                          gameState[(x    ) % numberX_cells, (y - 1) % numberY_cells] + \
+                          gameState[(x + 1) % numberX_cells, (y - 1) % numberY_cells] + \
+                          gameState[(x - 1) % numberX_cells, (y    ) % numberY_cells] + \
+                          gameState[(x + 1) % numberX_cells, (y    ) % numberY_cells] + \
+                          gameState[(x - 1) % numberX_cells, (y + 1) % numberY_cells] + \
+                          gameState[(x    ) % numberX_cells, (y + 1) % numberY_cells] + \
+                          gameState[(x + 1) % numberX_cells, (y + 1) % numberY_cells]
+    
+                # ----- REGLAS DEL JUEGO DE LA VIDA -----
+    
+                # Regla #1: Una célula muerta con exactamente 3 células vecinas vivas "revive".
+                if gameState[x, y] == 0 and n_neigh == 3:
+                    new_gameState[x, y] = 1
+    
+                # Regla #2: Una célula viva con 2 o 3 células vecinas vivas sigue viva, en otro caso muere.
+                elif gameState[x, y] == 1 and (n_neigh < 2 or n_neigh > 3):
+                    new_gameState[x, y] = 0
+                
             # Dibujamos las condiciones anteriores
             poly = [
-                ((x) * dimensionCell_width, (y) * dimensionCell_height),
-                ((x + 1) * dimensionCell_width,     (y) * dimensionCell_height),
-                ((x + 1) * dimensionCell_width,     (y + 1) * dimensionCell_height),
-                ((x) * dimensionCell_width, (y + 1) * dimensionCell_height)
+                ((x)     * dimensionCell_width, (y)     * dimensionCell_height),
+                ((x + 1) * dimensionCell_width, (y)     * dimensionCell_height),
+                ((x + 1) * dimensionCell_width, (y + 1) * dimensionCell_height),
+                ((x)     * dimensionCell_width, (y + 1) * dimensionCell_height)
             ]
 
-            # Dibujando rejilla con el automata actualizado
-            pygame.draw.polygon(screen, color_cell, poly, int(abs(1 - new_gameState[x, y])))
-
+            # Dibujando rejilla con el automata actualizado.
+            # Se dibuja la celda para cada par x e y
+            if new_gameState[x, y] == 0:
+                pygame.draw.polygon(screen, color_cell, poly, 1)
+            else:
+                pygame.draw.polygon(screen, color_cell, poly, 0)
+                
     # Actualizamos el estado del juego. 
     gameState = new_gameState
-    time.sleep(0.1)
+
     pygame.display.flip()
 
